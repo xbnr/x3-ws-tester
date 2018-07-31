bdb2cd580aaff80e27d1c45717c002a2c95acc96
Franck Depoortere <franck.depoortere@sage.com>
2018-07-31 16:22:27 +0200

    proto mongobd

---
+++ ConsoleWSTester/Plugins/MongoDb/FsFiles.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTester.Plugins.MongoDb
+{
+    class FsFiles
+    {
+           [BsonId]
+            public string _id { get; set; }
+            public string filename { get; set; } = string.Empty;
+            public DateTime updatedOn { get; set; } = DateTime.Now;
+            public DateTime createdOn { get; set; } = DateTime.Now;
+            public int length { get; set; } = 0;
+
+    }
+}
