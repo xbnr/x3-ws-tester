3911fc1bd6174a72c280baa54ad61e0b5dfefda2
Eric Boureau <eric.boureau@sage.com>
2019-01-08 10:22:53 +0100

    fix vulnerability in dependencies

--- ConsoleTesterJS/app.js
+++ ConsoleTesterJS/app.js
@@ -1,8 +1,8 @@
-const electron = require('electron')
+const electron = require('electron');
 // Module to control application life.
-const app = electron.app
+const app = electron.app;
 // Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow
+const BrowserWindow = electron.BrowserWindow;
 
 // require('crash-reporter').start();
 
@@ -18,7 +18,7 @@ function createWindow() {
     mainWindow = new BrowserWindow({
         width: 800,
         height: 600
-    })
+    });
 
     splashScreen = new BrowserWindow({
         width: 400,
@@ -26,12 +26,12 @@ function createWindow() {
         transparent: true,
         show: false,
         frame: false
-    })
+    });
 
     // and load the index.html of the app.
-    mainWindow.loadURL(`file://${__dirname}/index.html`)
+    mainWindow.loadURL(`file://${__dirname}/index.html`);
 
-    splashScreen.loadURL(`file://${__dirname}/info.html`)
+    splashScreen.loadURL(`file://${__dirname}/info.html`);
 
     // Open the DevTools.
     // mainWindow.webContents.openDevTools()
@@ -42,38 +42,38 @@ function createWindow() {
         // in an array if your app supports multi windows, this is the time
         // when you should delete the corresponding element.
         mainWindow = null
-    })
+    });
+
+    mainWindow.once('ready-to-show', function () {
+
+        setTimeout(function () {
+            splashScreen.show();
+            setTimeout(function () { splashScreen.hide(); }, 3000);
+        });
+    }, 1000);
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
-
-mainWindow.once('ready-to-show', function () {
-
-    setTimeout(function () {
-        splashScreen.show();
-        setTimeout(function () { splashScreen.hide(); }, 3000);
-    });
-}, 1000);
+app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
     // On OS X it is common for applications and their menu bar
     // to stay active until the user quits explicitly with Cmd + Q
     if (process.platform !== 'darwin') {
-        app.quit()
+        app.quit();
     }
-})
+});
 
 app.on('activate', function () {
     // On OS X it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (mainWindow === null) {
-        createWindow()
+        createWindow();
     }
-})
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
