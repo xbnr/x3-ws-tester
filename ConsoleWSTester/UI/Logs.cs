1f404afe699aaf19bd34c456ccd91ae5effefd2d
Franck Depoortere <franck.depoortere@sage.com>
2019-04-16 13:54:48 +0200

    add GetVersion PrintServer

--- ConsoleWSTester/UI/Logs.cs
+++ ConsoleWSTester/UI/Logs.cs
@@ -1,15 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Drawing;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using WeifenLuo.WinFormsUI.Docking;
-using System.IO;
-using ConsoleTester.LogsAnalyzer;
 
 namespace ConsoleTester.UI
 {
@@ -38,7 +29,8 @@ namespace ConsoleTester.UI
 
         private void copyToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            Clipboard.SetText(LogControl.SelectedText);
+            if (!string.IsNullOrEmpty(LogControl.SelectedText))
+                Clipboard.SetText(LogControl.SelectedText);
         }
     }
 }
